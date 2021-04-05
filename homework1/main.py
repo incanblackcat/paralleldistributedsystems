5fd4fc6b141e602458d9a26cf315a3cc1bdf7b49
incanblackcat <rogelioabelardoaleman@gmail.com>
2021-04-04 21:40:50 -0400

    Modified and corrected flop calculator

--- homework1/main.py
+++ homework1/main.py
@@ -1,6 +1,7 @@
 import time
 import threading
 
+
 class thread(threading.Thread):
     __kill = False
     __count = 0
@@ -14,9 +15,8 @@ class thread(threading.Thread):
 
     def calculate(self):
         while not self.__kill:
-            time.sleep(self.__threadNumber % 1 + 2)
+            #time.sleep(self.__threadNumber % 1 + 2)
             self.__count += 1
-        print("count: " + str(self.__count))
 
     def get_Count(self):
         return self.__count
@@ -25,35 +25,46 @@ class thread(threading.Thread):
         self.calculate()
 
 
-def newThread(num):
+def newThread(num, sleepDur):
     allThreads = []
+    threadValues = []
+
     operations = 0
     for i in range(num):
         t = thread(i)
         t.start()
         allThreads.append(t)
 
+    print ('\nrunning')
+
+    time.sleep(sleepDur)
+
+
     for i in allThreads:
         i.kill()
+
+    print('joining all threads')
 
     for i in allThreads:
         i.join()
 
+
     for i in allThreads:
         operations += i.get_Count()
 
-    print('Operations: ' + str(operations))
+    print('Flops = ' + str(operations/sleepDur))
 
 
 def main():
-    print('Currently running 1 thread')
-    newThread(1)
-    print('Currently running 2 threads')
-    newThread(2)
-    print('Currently running 4 threads')
-    newThread(4)
-    print('Currently running 8 threads')
-    newThread(8)
+    time = int(input("How long would you like to run each thread execution? "))
+    print('\nCurrently running 1 thread')
+    newThread(1, time)
+    print('\nCurrently running 2 threads')
+    newThread(2,time)
+    print('\nCurrently running 4 threads')
+    newThread(4, time)
+    print('\nCurrently running 8 threads')
+    newThread(8, time)
 
 
 print()
